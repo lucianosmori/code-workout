b17b565f7a795d00902a2f4d220ad4610fb0c819
lucianosmori <2429912+lucianosmori@users.noreply.github.com>
2023-10-07 15:59:26 -0400

    Refactor to add pytest support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    version='0.0.1',
+    description='Python challenges',
+    author='Luciano',
+    packages=find_packages(),
+    install_requires=[
+        'pytest'
+    ],
+)
