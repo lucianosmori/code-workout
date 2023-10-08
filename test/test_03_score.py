7b82fcbada763c76bfe413eb30903b4d186fd833
lucianosmori <2429912+lucianosmori@users.noreply.github.com>
2023-10-08 11:26:36 -0400

    update decorator parameters

--- test/test_03_score.py
+++ test/test_03_score.py
@@ -1,10 +1,11 @@
 from challenges.score_03 import Solution
 import pytest
 
-@pytest.mark.parametrize("list,result", [
-    ([1,3,2], 2)
+@pytest.mark.parametrize("nums,result", [
+    ([1,3,2], 2),
+    ([4,9,7,1,3], 7)
 ])
-def test_03_score(list, result):
+def test_03_score(nums, result):
     s = Solution()
-    assert s.get_runner_score(list) == result
+    assert s.get_runner_score(nums) == result
     
