e875fb7ee64b723281f16f10033bb5c44f5b4ff5
lucianosmori <2429912+lucianosmori@users.noreply.github.com>
2023-10-08 11:22:19 -0400

    add challenge

---
+++ challenges/score_03.py
@@ -0,0 +1,12 @@
+class Solution:
+    def get_runner_score(self, scores: list[int]) -> int:
+        scores.sort()
+        return scores[-2]
+
+
+if __name__ == '__main__':
+    #n = int(input().split())
+    n = [1,3,2]
+    #arr = map(int, input().split())
+    s = Solution()
+    print(s.get_runner_score(n))
