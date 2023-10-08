804c178e68b2c8c4235b452eb25117e76c197ed1
lucianosmori <2429912+lucianosmori@users.noreply.github.com>
2023-10-08 16:09:06 -0400

    add one unit test back

--- test/test_03_score.py
+++ test/test_03_score.py
@@ -1,14 +1,14 @@
-# from challenges.score_03 import Solution
-# import pytest
+from challenges.score_03 import Solution
+import pytest
 
-# @pytest.mark.parametrize("nums,result", [
-#     ([1,3,2], 2),
-#     ([4,9,7,1,3], 7),
-#     ([2,3,6,6,5], 5),
-#     ([1,-1,-2,-1],-1),
-#     ([-5,-7,3,8,-44], 3),
-# ])
-# def test_03_score(nums, result):
-#     s = Solution()
-#     assert s.get_runner_score(nums) == result
+@pytest.mark.parametrize("nums,result", [
+    ([1,3,2], 2),
+    # ([4,9,7,1,3], 7),
+    # ([2,3,6,6,5], 5),
+    # ([1,-1,-2,-1],-1),
+    # ([-5,-7,3,8,-44], 3),
+])
+def test_03_score(nums, result):
+    s = Solution()
+    assert s.get_runner_score(nums) == result
     
